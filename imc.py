8b09c3e8c5bab16225ecde2a7c70adb73451e4ba
Kaique Pina <kaiquepinak@gmail.com>
2025-07-18 00:10:26 -0300

    início do projeto de cálculo de massa corporal

--- imc.py
+++ imc.py
@@ -25,4 +25,3 @@ print("Obrigado por usar a calculadora de IMC!")
 #Fim do programa
 #adicionar um comentário para o git commit
 #git commit -m “início do projeto de cálculo de massa corporal"
-#fim    
